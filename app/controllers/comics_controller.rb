d045618ee287df255a4a9af6be43526e4c84abac
Romain Bruzeau <bruzeau.romain@gmail.com>
2024-02-27 15:02:16 +0100

    Controller-routes-comics-all-def

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,6 +1,6 @@
 class ComicsController < ApplicationController
   # skip_before_action
-  # before_action
+  before_action set_comic, only: %i[ show edit update destroy ]
 
   def index
     @comics = Comic.all
@@ -13,19 +13,37 @@ class ComicsController < ApplicationController
     @comic = Comic.new
   end
 
-  def create
-  end
-
   def edit
   end
 
+  def create
+    @comic = Comic.new(comic_params)
+    if @comic.save
+      redirect_to @comic, notice: "Comic was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def update
+    if @comic.update(comic_params)
+      redirect_to @comic, notice: "Comic was successfully updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @comic.destroy!
+    redirect_to comics_url, notice: "Comic was successfully destroyed.", status: :see_other
   end
 
   private
+  def set_comic
+    @comic = Comic.find(params[:id])
+  end
 
-
+  def comic_params
+    params.require(:comic).permit(:title, :author, :genre)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,30 @@
 Rails.application.routes.draw do
-  get 'comics/index'
-  get 'comics/show'
-  get 'comics/create'
-  get 'comics/edit'
-  get 'comics/update'
-  get 'comics/destroy'
-  get 'profiles/edit'
-  get 'profiles/update'
-  get 'profiles/destroy'
-  get 'bookings/index'
-  get 'bookings/show'
-  get 'bookings/create'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'bookings/destroy'
+
   devise_for :users
   root to: "pages#home"
+
+  # get 'comics/index'
+  # get 'comics/show'
+  # get 'comics/create'
+  # get 'comics/edit'
+  # get 'comics/update'
+  # get 'comics/destroy'
+  resources :comics do
+    resources :bookings, only: [ :new, :create ]
+  end
+
+  # get 'profiles/update'
+  # get 'profiles/destroy'
+  resources :profiles, only: [ :update, :destroy ]
+
+  # get 'bookings/index'
+  # get 'bookings/show'
+  # get 'bookings/create'
+  # get 'bookings/edit'
+  # get 'bookings/update'
+  # get 'bookings/destroy'
+  resources :bookings, only: [ :show ]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

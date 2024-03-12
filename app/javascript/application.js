7c0aa032f93037970f870c490fcb7889c8055ce9
Abdelkalek Chahed <ka.chahed@gmail.com>
2024-03-12 21:02:59 +0100

    test

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,16 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+document.addEventListener('DOMContentLoaded', function() {
+  const addButton = document.getElementById('add-comic-button');
+  const addForm = document.getElementById('add-comic-form');
+
+  addButton.addEventListener('click', function() {
+    if (addForm.style.display === 'none') {
+      addForm.style.display = 'block';
+    } else {
+      addForm.style.display = 'none';
+    }
+  });
+});
